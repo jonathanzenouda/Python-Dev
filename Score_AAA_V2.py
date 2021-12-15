b1a4bf59a88b0e474aaa762ce8f1cbe36be80a40
jonathanzenouda <62285593+jonathanzenouda@users.noreply.github.com>
2021-12-15 13:19:34 +0100

    Update to viyawaves + AutoML

--- Score_AAA_V2.py
+++ Score_AAA_V2.py
@@ -9,9 +9,9 @@ st.title("Interface de scoring Look-a-like")
 with st.sidebar:
     with st.form("my_form"):
         st.header("Connection")
-        user=st.text_input(label="Nom d'utilisateur", value="sastest1")
-        mdp=st.text_input(label="Mot de passe", value="Go4thsas",type="password")
-        cas_host ='https://aaa2021.frafed-yl1-azure-nginx-ceafe719.unx.sas.com/cas-shared-default-http/'
+        user=st.text_input(label="Nom d'utilisateur", value="frajoz")
+        mdp=st.text_input(label="Mot de passe", value="y42tCohuVh4D",type="password")
+        cas_host ='https://viyawaves.sas.com/cas-shared-default-http/'
         cas_port= 80
         st.header("Sélection du fichier")
         path=st.file_uploader(label="Parcourir")
@@ -27,9 +27,10 @@ with st.sidebar:
             indata="BAL_LYON_MATCHED"
             print(indata)
             outdata="BAL_TOULON"
-            op = s.table.loadTable(path=str(indata).upper()+".sashdat", casout={"name":indata, "replace":True})
-            print(op)
-            op2 = s.table.loadTable(path=str(outdata).upper()+".sashdat", casout={"name":outdata, "replace":True})
+            if s.table.tableExists(indata)==0:
+                op = s.table.loadTable(path=str(indata).upper()+".sashdat", casout={"name":indata, "replace":True})
+            if s.table.tableExists(outdata)==0:
+                op2 = s.table.loadTable(path=str(outdata).upper()+".sashdat", casout={"name":outdata, "replace":True})
             s.datastep.runcode('data '+outdata+'_MATCHED;set '+outdata+';voie_nom_match=dqmatch ("voie_nom"n, "Address (Street Only)", 90, "FRFRA");run;')
             s.builtins.loadActionSet("fedSql")
             s.fedsql.execDirect(query='''create table INMODEL{options replace=true} as 
@@ -38,13 +39,34 @@ with st.sidebar:
             inmodel=s.datastep.runcode('data INMODEL;set INMODEL;if missing(voie_nom_1) then CIBLE=0; else CIBLE=1;run; ')
             print(inmodel)
             viewdf=s.CASTable('INMODEL').nlargest(n=5,columns=['CIBLE'])
+            print('AutoML')
+            s.loadactionset('dataSciencePilot')
+            trans_out = dict(name = 'trans_out', replace=True)
+            feat_out = dict(name = 'feat_out', replace=True)
+            pipeline_out = dict(name = 'pipeline_out', replace=True)
+            #save_state_out = dict(name = 'save_state_out', replace=True)
+            automl_model = dict(name = 'automl_model', replace=True) 
+            automl=s.dataSciencePilot.dsAutoMl(table=ech_apprent, target=target,kFolds=5,
+                            modelTypes=["LOGISTIC","FOREST","GRADBOOST","DECISIONTREE", "NEURALNET"], 
+                            transformationOut = trans_out,
+                            featureOut = feat_out,
+                            pipelineOut = pipeline_out,
+                            objective='AUC',
+                            saveState = automl_model)
+            pipelinedf=s.CASTable('pipeline_out').nlargest(n=10,columns=['Objective'])
+            featdf=s.CASTable('feat_out').nlargest(n=10,columns=['FeatureId'])
+            print('AutoML done')
 
 try:
     st.table(viewdf)
+    st.table(pipelinedf)
+    st.table(featdf)
     print('ON EST LES CHAMPIONS')
+    
+    s.terminate()
 except:
     print('ERROR')
     
     
-s.terminate()
+
             
