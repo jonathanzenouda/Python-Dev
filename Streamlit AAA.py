0c2a8f2b111ddd6780b53514fb34d020baa04c1e
jonathanzenouda <62285593+jonathanzenouda@users.noreply.github.com>
2021-12-15 17:16:31 +0100

    Add Plotly

--- Streamlit AAA.py
+++ Streamlit AAA.py
@@ -3,6 +3,7 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import swat
 from getpass import getpass
 import streamlit as st
+import plotly.express as px
 
 st.title("Interface de scoring Look-a-like")
 
@@ -15,15 +16,17 @@ with st.sidebar:
         cas_port= 80
         st.header("Sélection du fichier")
         path=st.file_uploader(label="Parcourir")
-        submitted = st.form_submit_button("Exécuter")
-        st.header("Sélection des paramètres")
+        st.header("Réconciliation des bases de données")
+        var_match=st.text_input(label="Variable à matcher",value="voie_nom")
+        type_match=st.selectbox(label="Type de match",options=("Address (Street Only)","Address","Name","Account Number","Date (DMY)","Date (YMD)","Date (MDY)","Organization","Phone","Text"))
+        st.header("AutoML")
         st.number_input(label="Nombre de lignes en sortie")
+        submitted = st.form_submit_button("Exécuter")
         if submitted:
             s = swat.CAS(cas_host, cas_port, user, mdp)
-            print(s)
-            df=pd.read_csv(path)
-            s.upload_frame(df,casout={'name':'totrain','caslib':'CASUSER','replace':True})
             s.sessionprop.setsessopt(caslib='Public',timeout=31536000)
+            print(s)
+            totrain=s.read_csv(path,sep=";",casout='TOTRAIN')
             indata="BAL_LYON_MATCHED"
             print(indata)
             outdata="BAL_TOULON"
@@ -31,41 +34,47 @@ with st.sidebar:
                 op = s.table.loadTable(path=str(indata).upper()+".sashdat", casout={"name":indata, "replace":True})
             if s.table.tableExists(outdata)==0:
                 op2 = s.table.loadTable(path=str(outdata).upper()+".sashdat", casout={"name":outdata, "replace":True})
-            s.datastep.runcode('data '+outdata+'_MATCHED;set '+outdata+';voie_nom_match=dqmatch ("voie_nom"n, "Address (Street Only)", 90, "FRFRA");run;')
+            s.datastep.runcode('data '+outdata+'_MATCHED;set '+outdata+';voie_nom_match=dqmatch ("'+var_match+'"n, "'+type_match+'", 90, "FRFRA");run;')
+            print("matchcode end")
             s.builtins.loadActionSet("fedSql")
             s.fedsql.execDirect(query='''create table INMODEL{options replace=true} as 
             select distinct t1.*,t2.voie_nom as voie_nom_1
             from '''+indata+''' t1 left join '''+outdata+'''_MATCHED t2 on t1.voie_nom_match=t2.voie_nom_match  ''')
             inmodel=s.datastep.runcode('data INMODEL;set INMODEL;if missing(voie_nom_1) then CIBLE=0; else CIBLE=1;run; ')
-            print(inmodel)
+            print("fedsql end")
             viewdf=s.CASTable('INMODEL').nlargest(n=5,columns=['CIBLE'])
-            print('AutoML')
+            print('AutoML begin')
             s.loadactionset('dataSciencePilot')
             trans_out = dict(name = 'trans_out', replace=True)
             feat_out = dict(name = 'feat_out', replace=True)
             pipeline_out = dict(name = 'pipeline_out', replace=True)
             #save_state_out = dict(name = 'save_state_out', replace=True)
             automl_model = dict(name = 'automl_model', replace=True) 
-            automl=s.dataSciencePilot.dsAutoMl(table=ech_apprent, target=target,kFolds=5,
-                            modelTypes=["LOGISTIC","FOREST","GRADBOOST","DECISIONTREE", "NEURALNET"], 
+            s.loadactionset(actionset="sampling")
+            s.stratified(display={"names":"STRAFreq"},output={"casOut":{"name":"samptrain","replace":True}, "copyVars":"ALL"},samppct=3,partind=False, seed=10,table={"name":"totrain", "groupBy":{"CIBLE"}},outputTables={"names":{"STRAFreq"},"replace":True})
+            automl=s.dataSciencePilot.dsAutoMl(table="samptrain", target="CIBLE",kFolds=3,
+                            modelTypes=["DECISIONTREE","GRADBOOST"], 
                             transformationOut = trans_out,
                             featureOut = feat_out,
                             pipelineOut = pipeline_out,
                             objective='AUC',
                             saveState = automl_model)
-            pipelinedf=s.CASTable('pipeline_out').nlargest(n=10,columns=['Objective'])
+            s.datastep.runcode('data pipeline_graph;set pipeline_out;PIPELINE=PipelineID||"_"||modelType;keep PIPELINE Objective;rename Objective=AUC;run;')
+            pipelinedf=s.CASTable('pipeline_graph').nlargest(n=10,columns=['AUC'])
             featdf=s.CASTable('feat_out').nlargest(n=10,columns=['FeatureId'])
             print('AutoML done')
 
 try:
     st.table(viewdf)
-    st.table(pipelinedf)
+    fig = px.bar(pipelinedf, x='PIPELINE', y='AUC',orientation='h')
+    st.plotly_chart(fig, use_container_width=True)
+
     st.table(featdf)
     print('ON EST LES CHAMPIONS')
     
     s.terminate()
 except:
-    print('ERROR')
+    print("ERROR")
     
     
 
